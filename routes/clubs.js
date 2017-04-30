5930b1c3b6664529a28fb7961d77ddbc42df75cc
ybyGold <415098820@qq.com>
2017-04-30 18:25:56 -0500

    no message

--- routes/clubs.js
+++ routes/clubs.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var dbPath = "mongodb://admin:admin@ds127391.mlab.com:27391/rider2yby";
-var db = mongojs(dbPath);
 router.get('/', (req, res) => {
     let long = parseFloat(req.query.long);
     let lat = parseFloat(req.query.lat);
